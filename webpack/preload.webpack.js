ebab0fa8709f8ef83f5c532a1a8bb09784f50fb3
wotupfoo <edward@edwardandalison.com>
2026-03-29 21:30:37 -0700

    Adding the ability to run locally using "npm start"

---
+++ webpack/preload.webpack.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+    target: 'electron-preload',
+    externalsPresets: {
+        electronPreload: true,
+        node: true,
+    },
+    externals: {
+        assert: 'commonjs2 assert',
+        crypto: 'commonjs2 crypto',
+        electron: 'commonjs2 electron',
+        fs: 'commonjs2 fs',
+        os: 'commonjs2 os',
+        path: 'commonjs2 path',
+        util: 'commonjs2 util',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: require('./rules.webpack'),
+    },
+};
